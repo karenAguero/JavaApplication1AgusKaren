bf119d0dfdb362a70ca91ef31612474d327b21a3
Us <Us@Lab-22>
2022-07-26 10:11:26 -0300

    123

--- src/javaapplication1/agus/karen/JavaApplication1AgusKaren.java
+++ src/javaapplication1/agus/karen/JavaApplication1AgusKaren.java
@@ -16,6 +16,7 @@ public class JavaApplication1AgusKaren {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("-----");
+        System.out.print("-----");
     }
     
 }
