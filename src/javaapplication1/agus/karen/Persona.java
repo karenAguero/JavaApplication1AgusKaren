cff27e1bd7f03efbefcec1c9dab233df4fe11830
KA <KA@Lab-24>
2022-08-02 10:22:30 -0300

    2

---
+++ src/javaapplication1/agus/karen/Persona.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1.agus.karen;
+
+/**
+ *
+ * @author KA
+ */
+public class Persona {
+    
+    private String SEXO = "hombre";
+    
+    private String nombre; 
+    private int edad; 
+    private int dni; 
+    private String sexo; 
+    private double peso;
+    private double altura; 
+ 
+    public Persona () {
+        nombre = "";       
+        edad = 0;
+        dni = 0;
+        sexo = SEXO;
+        peso = 0.0;
+        altura =0.0;
+    }     
+     public Persona (String valorNombre,int valorEdad,String valorSexo) {
+        nombre = valorNombre;       
+        edad = valorEdad;
+        dni = 0;
+        sexo = valorSexo;
+        peso = 0.0;
+        altura =0.0;                   
+    }     
+     public Persona (String valorNombre,int valorEdad,String valorSexo, int valorDni, Double valorPeso, Double valorAltura) {
+        nombre = valorNombre;       
+        edad = valorEdad;
+        dni = valorDni;
+        sexo = valorSexo;
+        peso = valorPeso;
+        altura = valorAltura;                   
+    }     
+    public void setNombre (String valorNombre) {
+        nombre = valorNombre; 
+    } 
+    public void setEdad (int valorEdad) {
+        edad = valorEdad; 
+    } 
+    public void setDni (int valorDni) {
+        dni = valorDni; 
+    } 
+    public void setSexo (String valorSexo) {
+        sexo = valorSexo; 
+    } 
+    public void setPeso (Double valorPeso) {
+        peso = valorPeso; 
+    } 
+    public void setAltura (Double valorAltura) {
+        altura = valorAltura; 
+    } 
+    
+    public String getNombre () { return nombre; } 
+    public int getEdad () { return edad; } 
+    public int getDni () { return dni; }
+    public String getSexo () { return sexo; }
+    public Double getPeso () { return peso; }
+    public Double getAltura () { return altura; }
+} 
+
